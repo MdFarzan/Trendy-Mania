7d5eb39ca8229b382fe0b547bfd4ad3347749529
MdFarzan <mdfarzan776@gmail.com>
2022-03-10 20:16:23 +0530

    admin basic info & menu links added

--- admin/partials/sidebar.php
+++ admin/partials/sidebar.php
@@ -4,7 +4,7 @@
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">Trendy Mania</span>
     </a>
 
     <!-- Sidebar -->
@@ -12,10 +12,13 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          
+        
+        <!-- <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image"> -->
+        <img src="<?php echo $profile['avtar']==null ? "img/avtar/default-avtar.png": $profile['avtar']; ?>" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block"><?php echo $profile['name']; ?></a>
         </div>
       </div>
 
@@ -36,29 +39,110 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
+               <li class="nav-item">
+                <a href="dashboard.php" class="nav-link">
+                  <i class="nav-icon fas fa-tachometer-alt"></i>
+                  <p>
+                    Dashboard
+                  </p>
+                </a>
+              </li>
+
+              <!-- hero-banner management start -->
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <!-- <i class="nav-icon fas fa-tachometer-alt"></i> -->
+              <i class="nav-icon fas fa-image"></i>
+  <!-- continue from here -->
+              
+              <p>
+                Hero Banner
+                <i class="right fas fa-angle-left"></i>
+                
+
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="banners.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>All Banners</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="add-banner.php" class="nav-link active">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Add Banner</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <!-- hero-banner management ends -->
+              
+              <!-- category management start -->
           <li class="nav-item menu-open">
             <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <!-- <i class="nav-icon fas fa-tachometer-alt"></i> -->
+              <i class="nav-icon far fa-list-alt"></i>
               <p>
-                Starter Pages
+                Category Management
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
+                <a href="categories.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Active Page</p>
+                  <p>All Categories</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a href="add-category.php" class="nav-link active">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Inactive Page</p>
+                  <p>Add Category</p>
                 </a>
               </li>
             </ul>
           </li>
+          <!-- category management ends -->
+
+          <!-- product management starts -->
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <!-- <i class="nav-icon fas fa-tachometer-alt"></i> -->
+              <i class="nav-icon fas fa-boxes"></i>
+              <p>
+                Product Management
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="products.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>All Product</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="add-product.php" class="nav-link active">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Add Product</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <!-- product management ends -->
+          
+          <li class="nav-item">
+                <a href="logout.php" class="nav-link">
+                  <!-- <i class="nav-icon fas fa-tachometer-alt"></i> -->
+                  <i class="nav-icon fas fa-sign-out-alt" style="transform:rotate(180deg)"></i>
+                  <p>
+                    Logout
+                  </p>
+                </a>
+          </li>
+
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
